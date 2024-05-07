56766e5324210b3845362082208a5142630a4358
Alex Olieman <alex@olieman.net>
2024-05-07 16:30:29 +0200

    test cover retirement (positive)

---
+++ tests/test_association.py
@@ -0,0 +1,137 @@
+
+import pytest
+
+from sc_audit.loader import minted_blocks, retirement_from_block
+from sc_audit.loader.utils import VcsSerialNumber
+from tests.data_fixtures.retirements import get_retirements
+from tests.db_fixtures import new_session, vcs_project
+from tests.test_mint import mock_http
+
+
+class TestRetirementFromBlock:
+    def test_cover_retirement_block_start(self, mock_session_with_blocks):
+        retirement = get_retirements()[0]
+        serial_number = VcsSerialNumber.from_str(retirement.serial_number)
+        retirement.vcu_amount = 1
+        serial_number.block_start = 449402277
+        serial_number.block_end = 449402277
+        retirement.serial_number = serial_number.to_str()
+
+        with mock_session_with_blocks.begin() as session:
+            rfbs = retirement_from_block.cover_retirement(session, retirement)
+            assert len(rfbs) == 1
+            rfb: retirement_from_block.RetirementFromBlock = rfbs[0]
+            assert rfb.retirement_id == retirement.certificate_id
+            assert rfb.vcu_amount == retirement.vcu_amount
+            session.add(rfb)
+            session.flush()
+            assert rfb.block.block_start == serial_number.block_start
+            assert rfb.block.block_end != serial_number.block_end
+
+    def test_cover_retirement_block_end(self, mock_session_with_blocks):
+        retirement = get_retirements()[0]
+        serial_number = VcsSerialNumber.from_str(retirement.serial_number)
+        retirement.vcu_amount = 2
+        serial_number.block_start = 449402299
+        serial_number.block_end = 449402300
+        retirement.serial_number = serial_number.to_str()
+
+        with mock_session_with_blocks.begin() as session:
+            rfbs = retirement_from_block.cover_retirement(session, retirement)
+            assert len(rfbs) == 1
+            rfb: retirement_from_block.RetirementFromBlock = rfbs[0]
+            assert rfb.retirement_id == retirement.certificate_id
+            assert rfb.vcu_amount == retirement.vcu_amount
+            session.add(rfb)
+            session.flush()
+            assert rfb.block.block_start != serial_number.block_start
+            assert rfb.block.block_end == serial_number.block_end
+
+    def test_cover_retirement_block_middle(self, mock_session_with_blocks):
+        retirement = get_retirements()[0]
+        serial_number = VcsSerialNumber.from_str(retirement.serial_number)
+        retirement.vcu_amount = 3
+        serial_number.block_start = 449402280
+        serial_number.block_end = 449402282
+        retirement.serial_number = serial_number.to_str()
+
+        with mock_session_with_blocks.begin() as session:
+            rfbs = retirement_from_block.cover_retirement(session, retirement)
+            assert len(rfbs) == 1
+            rfb: retirement_from_block.RetirementFromBlock = rfbs[0]
+            assert rfb.retirement_id == retirement.certificate_id
+            assert rfb.vcu_amount == retirement.vcu_amount
+            session.add(rfb)
+            session.flush()
+            assert rfb.block.block_start != serial_number.block_start
+            assert rfb.block.block_end != serial_number.block_end
+
+    def test_cover_retirement_spans_two(self, mock_session_with_blocks):
+        retirement = get_retirements()[0]
+        serial_number = VcsSerialNumber.from_str(retirement.serial_number)
+        retirement.vcu_amount = 24
+        serial_number.block_start = 449402280
+        serial_number.block_end = 449402303
+        retirement.serial_number = serial_number.to_str()
+
+        with mock_session_with_blocks.begin() as session:
+            rfbs = retirement_from_block.cover_retirement(session, retirement)
+            assert len(rfbs) == 2
+            rfb_one, rfb_two = rfbs
+            for rfb in rfbs:
+                assert rfb.retirement_id == retirement.certificate_id
+                
+            assert rfb_one.vcu_amount == 21
+            assert rfb_two.vcu_amount == 3
+            session.add_all(rfbs)
+            session.flush()
+            assert rfb_one.block.block_start < serial_number.block_start < rfb_one.block.block_end
+            assert rfb_two.block.block_start < serial_number.block_end < rfb_two.block.block_end
+
+    def test_cover_retirement_spans_three(self, mock_session_with_blocks):
+        retirement = get_retirements()[0]
+        serial_number = VcsSerialNumber.from_str(retirement.serial_number)
+        retirement.vcu_amount = 32
+        serial_number.block_start = 449402289
+        serial_number.block_end = 449402320
+        retirement.serial_number = serial_number.to_str()
+
+        with mock_session_with_blocks.begin() as session:
+            rfbs = retirement_from_block.cover_retirement(session, retirement)
+            assert len(rfbs) == 3
+            rfb_one, rfb_two, rfb_three = rfbs
+            for rfb in rfbs:
+                assert rfb.retirement_id == retirement.certificate_id
+                
+            assert rfb_one.vcu_amount == 12
+            assert rfb_two.vcu_amount == 16
+            assert rfb_three.vcu_amount == 4
+            session.add_all(rfbs)
+            session.flush()
+            assert rfb_one.block.block_start < serial_number.block_start < rfb_one.block.block_end
+            assert rfb_two.vcu_amount == rfb_two.block.size
+            assert rfb_three.block.block_start < serial_number.block_end < rfb_three.block.block_end
+
+    def test_cover_retirement_overspent(self, mock_session_with_blocks):
+        # TODO
+        pass
+
+    def test_cover_retirement_uncovered(self, mock_session_with_blocks):
+        # TODO
+        pass
+
+
+@pytest.fixture
+def mock_session(monkeypatch, new_session):
+    monkeypatch.setattr(minted_blocks, 'Session', new_session)
+    monkeypatch.setattr(retirement_from_block, 'Session', new_session)
+    return new_session
+
+@pytest.fixture
+def mock_session_with_blocks(mock_http, mock_session, vcs_project):
+    with mock_session.begin() as session:
+        session.add(vcs_project)
+        session.add_all(get_retirements())
+
+    minted_blocks.load_minted_blocks()
+    return mock_session
