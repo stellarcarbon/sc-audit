6cdac3308a31102be7be151369b81a4d83f1f714
Alex Olieman <alex@olieman.net>
2024-06-06 13:27:35 +0200

    print DBAPI to stderr

--- sc_audit/session_manager.py
+++ sc_audit/session_manager.py
@@ -3,11 +3,12 @@ DB connection management.
 
 Author: Alex Olieman <https://keybase.io/alioli>
 """
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from sc_audit.constants import DBAPI_URL
 
-print(f"Connecting to database {DBAPI_URL}...")
+print(f"Connecting to database {DBAPI_URL}...", file=sys.stderr)
 engine = create_engine(DBAPI_URL)
 Session = sessionmaker(engine)
