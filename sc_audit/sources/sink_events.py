d23d82a1156cb2c7c64f5d4353fc642aa11a5796
Alex Olieman <alex@olieman.net>
2025-05-09 21:17:37 +0200

    feat: fetch sink events from retroshades

---
+++ sc_audit/sources/sink_events.py
@@ -0,0 +1,60 @@
+"""
+Fetch sink events from Mercury Retroshades.
+
+Author: Alex Olieman <https://keybase.io/alioli>
+"""
+
+from decimal import Decimal
+
+import httpx
+from pydantic import BaseModel
+from stellar_sdk.sep.toid import TOID
+from sc_audit.config import settings
+
+UNIT_IN_STROOPS = 10_000_000
+
+
+class SinkEvent(BaseModel):
+    transaction: str
+    # TODO: created_at
+    contract_id: str
+    funder: str
+    recipient: str
+    amount: Decimal
+    project_id: str
+    memo_text: str | None
+    email: str | None
+    ledger: int
+
+    @classmethod
+    def from_raw(cls, **data):
+        data["amount"] = data["amount"] / UNIT_IN_STROOPS
+        return cls(**data)
+
+
+def get_sink_events(cursor: int=settings.FIRST_SINK_CURSOR) -> list[SinkEvent]:
+    headers = {
+        "Authorization": settings.MERCURY_KEY,
+        "Content-Type": "application/json",
+    }
+    ledger_from_toid = TOID.from_int64(cursor).ledger_sequence
+    with httpx.Client(headers=headers) as client:
+        payload = {
+            "query": f"""
+                SELECT * FROM sink_event{settings.RETROSHADES_MD5}
+                WHERE ledger > {ledger_from_toid}
+            """
+        }
+        resp: httpx.Response = client.post(
+            url=str(settings.RETROSHADES_URL),
+            json=payload,
+        )
+        resp.raise_for_status()
+        events = resp.json()
+
+    return [SinkEvent.from_raw(**event) for event in events]
+
+
+if __name__ == "__main__":
+    sink_events = get_sink_events(cursor=0)
+    pass
