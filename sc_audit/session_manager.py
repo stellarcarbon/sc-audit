df2af8a1dcab4e35d852530fe4a6c450825a984d
Alex Olieman <alex@olieman.net>
2025-06-14 19:43:16 +0200

    perf: move test mappers to migrations.env

--- sc_audit/session_manager.py
+++ sc_audit/session_manager.py
@@ -10,7 +10,6 @@ from sqlalchemy.engine.url import make_url
 from sqlalchemy.orm import sessionmaker
 
 from sc_audit.config import settings
-from sc_audit.db_schema.base import create_test_mappers
 
 print(f"Connecting to database {settings.DBAPI_URL}...", file=sys.stderr)
 
@@ -28,7 +27,3 @@ if db_url.get_backend_name() == "sqlite":
 
 engine = create_engine(settings.DBAPI_URL, connect_args=connect_args)
 Session = sessionmaker(engine)
-
-# create test mappers after all models have been registered
-# TODO: this only needs to happen when running alembic; it would be nice to check
-create_test_mappers()
